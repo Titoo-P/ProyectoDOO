87ac8ad27d04ff09e43e2f78cd91bc43b1b8a025
Roberto <pantostado1601@gmail.com>
2024-07-08 01:00:18 -0400

    fix: Main // Actualizado las variables que se piden

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,23 @@
-package backend;
+
+import backend.*;
+import GUI.*;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SistemaDeReservas sistemaDeReservas = new SistemaDeReservas();
+
+        // Agregar algunos autobuses para la demostración
+        sistemaDeReservas.agregarAutobus(new Autobus("001", "Ruta 1", "10:00 AM", 15, 10, 2500, 5000));
+        sistemaDeReservas.agregarAutobus(new Autobus("002", "Ruta 2", "12:00 PM", 20, 10, 2700, 5500));
+
+        // Iniciar GUI de selección de autobús
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new BusSeleccionGUI(sistemaDeReservas).setVisible(true);
+            }
+        });
     }
 }
