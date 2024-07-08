2677be1adc64c62b8bb1b20904b5f0bdc37c15e5
Roberto <pantostado1601@gmail.com>
2024-07-08 03:20:18 -0400

    Added: Autobus // Se cambio en gran medida autobus, esto para supportear el sistema para buses en 2 asientos

--- src/main/java/backend/Autobus.java
+++ src/main/java/backend/Autobus.java
@@ -7,20 +7,40 @@ public class Autobus {
     private String id;
     private String ruta;
     private String horario;
+    private int numeroDeAsientosSemiCama;
+    private int numeroDeAsientosSalonCama;
+    private int precioSemiCama;
+    private int precioSalonCama;
     private List<Asiento> asientos;
+    private int numeroDePisos;
 
-    public Autobus(String id, String ruta, String horario, int numeroDeAsientosSemiCama, int numeroDeAsientosSalonCama, int precioSemiCama, int precioSalonCama) {
+    public Autobus(String id, String ruta, String horario, int numeroDeAsientosSemiCama, int numeroDeAsientosSalonCama, int precioSemiCama, int precioSalonCama,int numeroDePisos) {
+        if (numeroDeAsientosSemiCama % 2 != 0) {
+            throw new IllegalArgumentException("El número de asientos semi-cama debe ser par.");
+        }
         this.id = id;
         this.ruta = ruta;
         this.horario = horario;
-        this.asientos = new ArrayList<>(numeroDeAsientosSemiCama + numeroDeAsientosSalonCama);
+        this.numeroDeAsientosSemiCama = numeroDeAsientosSemiCama;
+        this.numeroDeAsientosSalonCama = numeroDeAsientosSalonCama;
+        this.precioSemiCama = precioSemiCama;
+        this.precioSalonCama = precioSalonCama;
+        this.numeroDePisos = numeroDePisos;
+        this.asientos = new ArrayList<>();
+        crearAsientos();
+    }
 
+    private void crearAsientos() {
         for (int i = 1; i <= numeroDeAsientosSemiCama; i++) {
             asientos.add(new Asiento(i, "Semi Cama", precioSemiCama));
         }
-        for (int i = 1; i <= numeroDeAsientosSalonCama; i++) {
-            asientos.add(new Asiento(numeroDeAsientosSemiCama + i, "Salón Cama", precioSalonCama));
+        for (int i = numeroDeAsientosSemiCama + 1; i <= numeroDeAsientosSemiCama + numeroDeAsientosSalonCama; i++) {
+            asientos.add(new Asiento(i, "Salón Cama", precioSalonCama));
         }
+    }
+
+    public int getNumeroDePisos() {
+        return numeroDePisos;
     }
 
     public String getId() {
