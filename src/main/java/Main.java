5306fe3a247028c45732e591677a967751e4110b
Roberto <pantostado1601@gmail.com>
2024-07-08 05:46:09 -0400

    Actualizado Main para portear el sistema de informes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,8 +9,11 @@ public class Main {
         SistemaDeReservas sistemaDeReservas = new SistemaDeReservas();
 
         // Agregar algunos autobuses para la demostración
-        sistemaDeReservas.agregarAutobus(new Autobus("001", "Ruta 1", "10:00 AM", 16, 10, 2500, 5000, 1));
+        sistemaDeReservas.agregarAutobus(new Autobus("001", "Ruta 1", "10:00 AM", 30, 10, 2500, 5000, 1));
         sistemaDeReservas.agregarAutobus(new Autobus("002", "Ruta 2", "12:00 PM", 20, 10, 2700, 5500,2));
+
+        // Añadir un hook de cierre para generar el informe
+        Runtime.getRuntime().addShutdownHook(new Thread(sistemaDeReservas::generarReporte));
 
         // Iniciar GUI de selección de autobús
         SwingUtilities.invokeLater(new Runnable() {
