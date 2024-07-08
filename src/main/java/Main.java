0df742dede86242dc3a76d7b5179501a1bc8fb89
Roberto <pantostado1601@gmail.com>
2024-07-08 03:22:22 -0400

    fix: Main // actualizado para llamado de segundo piso

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,8 +9,8 @@ public class Main {
         SistemaDeReservas sistemaDeReservas = new SistemaDeReservas();
 
         // Agregar algunos autobuses para la demostración
-        sistemaDeReservas.agregarAutobus(new Autobus("001", "Ruta 1", "10:00 AM", 15, 10, 2500, 5000));
-        sistemaDeReservas.agregarAutobus(new Autobus("002", "Ruta 2", "12:00 PM", 20, 10, 2700, 5500));
+        sistemaDeReservas.agregarAutobus(new Autobus("001", "Ruta 1", "10:00 AM", 16, 10, 2500, 5000, 1));
+        sistemaDeReservas.agregarAutobus(new Autobus("002", "Ruta 2", "12:00 PM", 20, 10, 2700, 5500,2));
 
         // Iniciar GUI de selección de autobús
         SwingUtilities.invokeLater(new Runnable() {
