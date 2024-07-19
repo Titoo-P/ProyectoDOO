a5d041d4397d13e77726d0865ef4aa0fec573d44
Roberto <pantostado1601@gmail.com>
2024-07-19 08:01:52 -0400

    Commit de todo los archivos faltantes

--- src/main/java/backend/autobuses/AutobusConcreto.java
+++ src/main/java/backend/autobuses/AutobusConcreto.java
@@ -1,7 +1,7 @@
 package backend.autobuses;
 
 import backend.Autobus;
-
+// Clase solamente para pruebas en Tests unitarios
 public class AutobusConcreto extends Autobus {
     public AutobusConcreto(String id, String ruta, String horario, int asientosSemiCama, int asientosSalonCama, int precioSemiCama, int precioSalonCama, int numeroDePisos) {
         super(id, ruta, horario, asientosSemiCama, asientosSalonCama, precioSemiCama, precioSalonCama, numeroDePisos);
